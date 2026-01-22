a396c59ed0f4dd72f1cea5216d4e1f9e45ede590
Dileep <dileepkumawat525@gmail.com>
2026-01-22 23:48:29 +0530

    Implemented the Window Part.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./app.scss"
 import Dock from "./components/Dock"
 import Nav from "./components/Nav"
+import MacWindow from "./components/windows/MacWindow"
 
 const App = () => {
   return (
     <main>
       <Nav />
       <Dock />
+      <MacWindow>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam aut, fugit eveniet est, dolorum veritatis, quam ducimus delectus quas nobis molestiae possimus aliquid! Minus reiciendis dicta sed adipisci ea nisi debitis aut voluptatum ut labore omnis optio molestias assumenda fuga rerum autem similique soluta suscipit maxime, mollitia corrupti necessitatibus architecto sapiente! Quidem fugiat sequi nesciunt.</p>
+      </MacWindow>
     </main>
   )
 }
